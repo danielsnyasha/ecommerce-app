3f4b47a0e0f59a4b99225486202e95d99ee94871
nyasha musanhu <danielsnyasha@outlook.com>
2023-02-16 14:12:57 +0200

    commit 10

--- nextjs-ecommerce/pages/admin/orders.js
+++ nextjs-ecommerce/pages/admin/orders.js
@@ -47,7 +47,7 @@ export default function AdminOrderScreen() {
             </li>
             <li>
               <Link href="/admin/orders">
-                <a className="font-bold">Orders</a>
+                <p className="font-bold">Orders</p>
               </Link>
             </li>
             <li>
@@ -102,7 +102,7 @@ export default function AdminOrderScreen() {
                       </td>
                       <td className="p-5">
                         <Link href={`/order/${order._id}`} passHref>
-                          <a>Details</a>
+                          <p>Details</p>
                         </Link>
                       </td>
                     </tr>
